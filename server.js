898880386d4bf940e2018a82aa9d759d414d4036
farid ghaderi <farid.51gh@gmail.com>
2022-04-16 17:02:42 +0430

    fixing some errors for deploying on heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 //specify app`s config file
 dotenv.config({ path: "./config.env" });
-//Connecting to Data base
+//Connecting to Database
 const connectionString = process.env.DATABASE_CONNECTION_STRING;
 const startApp = async () => {
   await mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
@@ -12,7 +12,7 @@ const startApp = async () => {
     useCreateIndex: true,
     useFindAndModify: false,
   });
-  app.listen(process.env.PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Application has been started successfully...");
   });
 };
